6ba6d6d2af861610dfcf967375e0beeacfeda1e3
vansh210105 <vanshptl210@gmail.com>
2025-03-17 22:29:08 +0530

    Initial commit

---
+++ flappybird.js
@@ -0,0 +1,167 @@
+// Board
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+let context;
+
+// Bird
+let birdWidth = 34;
+let birdHeight = 24;
+let birdX = boardWidth / 8;
+let birdY = boardHeight / 2;
+let birdImg;
+
+let bird = {
+    x: birdX,
+    y: birdY,
+    width: birdWidth,
+    height: birdHeight
+};
+
+// Pipes
+let pipeArray = [];
+let pipeWidth = 64;
+let pipeHeight = 512;
+let pipeX = boardWidth;
+let pipeY = 0;
+
+let topPipeImg;
+let bottomPipeImg;
+
+// Physics
+let velocityX = -2; // Pipes moving left
+let velocityY = 0; // Bird jump speed
+let gravity = 0.4;
+
+let gameOver = false;
+let score = 0;
+
+window.onload = function () {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d"); // For drawing on the board
+
+    // Load Bird Image
+    birdImg = new Image();
+    birdImg.src = "./flappybird.png";
+    birdImg.onload = function () {
+        context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    };
+
+    // Load Pipe Images
+    topPipeImg = new Image();
+    topPipeImg.src = "./toppipe.png";
+
+    bottomPipeImg = new Image();
+    bottomPipeImg.src = "./bottompipe.png";
+
+    requestAnimationFrame(update);
+    setInterval(placePipes, 1500); // Every 1.5 seconds
+    document.addEventListener("keydown", moveBird);
+};
+
+function update() {
+    requestAnimationFrame(update);
+    if (gameOver) {
+        return;
+    }
+
+    context.clearRect(0, 0, board.width, board.height);
+
+    // Bird Movement
+    velocityY += gravity;
+    bird.y = Math.max(bird.y + velocityY, 0); // Limit bird to top of the canvas
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+
+    if (bird.y > board.height) {
+        document.getElementById("dieSound").play();
+        gameOver = true;
+    }
+
+    // Pipes Movement
+    for (let i = 0; i < pipeArray.length; i++) {
+        let pipe = pipeArray[i];
+        pipe.x += velocityX;
+        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+
+        if (!pipe.passed && bird.x > pipe.x + pipe.width) {
+            score += 0.5; // 0.5 because there are two pipes in a set
+            pipe.passed = true;
+            document.getElementById("pointSound").play();
+        }
+
+        if (detectCollision(bird, pipe)) {
+            document.getElementById("hitSound").play();
+            gameOver = true;
+        }
+    }
+
+    // Clear pipes that go off-screen
+    while (pipeArray.length > 0 && pipeArray[0].x < -pipeWidth) {
+        pipeArray.shift();
+    }
+
+    // Display Score
+    context.fillStyle = "white";
+    context.font = "45px sans-serif";
+    context.fillText(score, 5, 45);
+
+    if (gameOver) {
+        context.fillText("GAME OVER", 80, 90);
+    }
+}
+
+function placePipes() {
+    if (gameOver) {
+        return;
+    }
+
+    let randomPipeY = pipeY - pipeHeight / 4 - Math.random() * (pipeHeight / 2);
+    let openingSpace = board.height / 4;
+
+    let topPipe = {
+        img: topPipeImg,
+        x: pipeX,
+        y: randomPipeY,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false
+    };
+    pipeArray.push(topPipe);
+
+    let bottomPipe = {
+        img: bottomPipeImg,
+        x: pipeX,
+        y: randomPipeY + pipeHeight + openingSpace,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false
+    };
+    pipeArray.push(bottomPipe);
+}
+
+function moveBird(e) {
+    if (e.code == "Space" || e.code == "ArrowUp" || e.code == "KeyX") {
+        // Play flap sound
+        document.getElementById("flapSound").play();
+
+        // Bird jump
+        velocityY = -6;
+
+        // Reset game on game over
+        if (gameOver) {
+            bird.y = birdY;
+            pipeArray = [];
+            score = 0;
+            gameOver = false;
+        }
+    }
+}
+
+function detectCollision(a, b) {
+    return a.x < b.x + b.width &&
+        a.x + a.width > b.x &&
+        a.y < b.y + b.height &&
+        a.y + a.height > b.y;
+}
